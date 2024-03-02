16289eea9a4b81651d20412bb0c7b1ac685fd6fe
username@gmail.com <Sneha.trupkane018@gmail.com>
2024-03-02 14:53:14 +0530

    sneha changed code from eclipse

--- src/Waits/implicitWait.java
+++ src/Waits/implicitWait.java
@@ -1,5 +1,47 @@
 package Waits;
 
-public class implicitWait {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class implicitWait {
+	
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+	   WebElement Email=driver.findElement(By.xpath("//*[@id='email']"));
+	   Email.sendKeys("ahsfdhsfdh@gmail.com");
+	   driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("sneha@21");
+	
+	   driver.findElement(By.xpath("//*[@name='login']")).click();
+	   System.out.println("this code is changed by sneha from eclipse");
+	   
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
